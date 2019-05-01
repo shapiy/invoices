47fdc312502dea639c878e9f1f62ab53cd017a82
Serhiy Hapiy <hapiy.serhiy@gmail.com>
2019-05-01 15:09:47 +0300

    Customize month via cli

--- invoices/__init__.py
+++ invoices/__init__.py
@@ -1,8 +1,10 @@
 """Main module."""
 import argparse
+import calendar
 import datetime
 import logging
 import tempfile
+from typing import Tuple
 
 import toml
 
@@ -10,6 +12,28 @@ from invoices import template, report
 from invoices.export import export_docx
 
 LOGGER = logging.getLogger(__name__)
+
+
+def month(value: str) -> datetime.date:
+    """Parse month."""
+    return datetime.datetime.strptime(value, "%Y-%m")
+
+
+def period(args) -> Tuple[datetime.date, datetime.date]:
+    """Compute report period from command line args."""
+    if args.month is not None:
+        start: datetime.date = args.month
+        _, last_day = calendar.monthrange(start.year, start.month)
+        since = start
+        until = start.replace(day=last_day)
+    else:
+        # Fall back to the previous month, if not specified.
+        today = datetime.date.today()
+        start = today.replace(day=1)
+        until = start - datetime.timedelta(days=1)
+        since = until.replace(day=1)
+
+    return since, until
 
 
 def main() -> None:
@@ -25,12 +49,10 @@ def main() -> None:
                         help='Output file')
     parser.add_argument('-f', '--force', action='store_true',
                         help='Overwrite output if it exists')
+    parser.add_argument('-m', '--month', type=month,
+                        help='Month to generate invoice for, as YYYY-MM')
     args = parser.parse_args()
-
-    today = datetime.date.today()
-    start = today.replace(day=1)
-    until = start - datetime.timedelta(days=1)
-    since = until.replace(day=1)
+    since, until = period(args)
 
     LOGGER.info('Generating last month''s invoice')
 
